efde3faab2ca2cb65487763c9c87b48cb7ec295a
niconicosette <niconicosette@gmail.com>
2025-09-08 20:11:42 -0500

    mochi-discord-bot initial layout

---
+++ script.js
@@ -0,0 +1,22 @@
+const channels = Array.from(document.getElementById('channels').children);
+const content = Array.from(document.getElementById('content').children);
+
+const changeTab = (tab) => {
+    content.forEach(child => {
+        child.id === tab ? child.classList.add('active') : child.classList.remove('active');
+    });
+    channels.forEach(child => {
+        child.getAttribute('data-tab') === tab ? child.classList.add('active') : child.classList.remove('active');
+    });
+};
+
+channels.forEach(tab => {
+    tab.addEventListener('click', (event) => {
+        changeTab(event.currentTarget.getAttribute('data-tab'));
+    })
+});
+
+//switch tab on load when #guide or #legalese is appended to url
+document.addEventListener('DOMContentLoaded', () => {
+    window.location.hash && changeTab(window.location.hash.substring(1));
+});
